cbea907f889799c435bf189561568c5f13f8961f
William M. Tsikata <tsikatawill@gmail.com>
2022-06-04 23:49:38 +0000

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,18 @@ import { FaBars } from "react-icons/fa";
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
   return (
-    <nav className={`navbar ${showMenu && "bg-white fixed w-full top-0"}`}>
+    <nav
+      className={`navbar overflow-hidden ${
+        showMenu && "bg-white fixed w-full top-0 overflow-visible"
+      }`}
+    >
       <div className="container py-5 flex md:items-center justify-between relative z-50">
         <Logo />
         <ul
-          className={`nav-links capitalize flex items-center flex-col absolute md:relative md:flex-row w-full md:w-fit bg-white md:bg-transparent justify-between border-t border-gray-500 md:gap-5  ${
-            showMenu ? "top-full left-0" : "-top-[300px] md:top-0"
+          className={`nav-links capitalize flex items-center whitespace-nowrap flex-col absolute md:relative md:flex-row w-full md:w-fit bg-white md:bg-transparent justify-between md:gap-5  ${
+            showMenu
+              ? "top-full border-t border-gray-500 left-0"
+              : "-top-[300px] md:top-0 md:left-0"
           }`}
         >
           <li className="cursor-pointer hover:sm:bg-slate-200 w-full text-center py-2">
